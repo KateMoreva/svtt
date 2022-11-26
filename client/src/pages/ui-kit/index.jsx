2f7d8723d8a55341a427dc342fb03bb10d1908df
Ekaterina Moreva <ekaterina.moreva@vk.team>
2022-11-27 00:34:19 +0300

    fix client

--- client/src/pages/ui-kit/index.jsx
+++ client/src/pages/ui-kit/index.jsx
@@ -9,6 +9,10 @@ import { blockResultSearch } from '../../mocks/blockResultSearch';
 import SimilarQueries from '../../components/ui/similarQueries';
 import { similarQueries } from '../../mocks/similarQueries';
 import { useWindowSize } from '../../hooks/useWindowSize';
+import TermsBlock from '../../components/ui/termsBlock';
+import { termsSuggestions } from '../../mocks/termsBlock';
+import ImageBlock from '../../components/ui/imageBlock';
+import { imageResult } from '../../mocks/imageBlock';
 
 const UiKit = () => {
   // TODO: вынести в класс с историей поиска
@@ -39,48 +43,54 @@ const UiKit = () => {
 
   const handleOnClickLanguageButton = useCallback(() => {
     languageButtonType === LANGUAGE_BUTTON_TYPE.english
-        ? setLanguageButtonType(LANGUAGE_BUTTON_TYPE.russian)
-        : setLanguageButtonType(LANGUAGE_BUTTON_TYPE.english);
+      ? setLanguageButtonType(LANGUAGE_BUTTON_TYPE.russian)
+      : setLanguageButtonType(LANGUAGE_BUTTON_TYPE.english);
   }, [languageButtonType]);
 
   const windowSize = useWindowSize();
-  const { viewType } = windowSize;
+  const {viewType} = windowSize;
 
   const handleOnClickItemSimilarQueries = (query) => {
     setInputSearchValue(query);
   };
 
   return (
-      <div className="ui-kit">
-        <div className="ui-kit__component">
-          <InputSearch
-              query={inputSearchValue}
-              onEnterDown={handleOnEnterDown}
-              onDeleteHistoryItem={handleOnDeleteHistoryItem}
-              history={inputSearchHistoryQueries}
-          />
-        </div>
-        <div className="ui-kit__component">
-          <SearchButton text="Search" onClick={() => console.log('Search button click!')} />
-        </div>
-        <div className="ui-kit__component">
-          <LanguageButton type={languageButtonType} onClick={handleOnClickLanguageButton} />
-        </div>
-        <div className="ui-kit__component">
-          <BlockResultSearch
-              title={blockResultSearch.searchItems[0].title}
-              link={blockResultSearch.searchItems[0].link}
-          />
-        </div>
-        <div className="ui-kit__component">
-          <SimilarQueries
-              title="Related searches"
-              queries={similarQueries}
-              onClickItem={handleOnClickItemSimilarQueries}
-              viewType={viewType}
-          />
-        </div>
+    <div className="ui-kit">
+      <div className="ui-kit__component">
+        <InputSearch
+          query={inputSearchValue}
+          onEnterDown={handleOnEnterDown}
+          onDeleteHistoryItem={handleOnDeleteHistoryItem}
+          history={inputSearchHistoryQueries}
+        />
       </div>
+      <div className="ui-kit__component">
+        <SearchButton text="Search" onClick={() => console.log('Search button click!')} />
+      </div>
+      <div className="ui-kit__component">
+        <LanguageButton type={languageButtonType} onClick={handleOnClickLanguageButton} />
+      </div>
+      <div className="ui-kit__component">
+        <BlockResultSearch
+          title={blockResultSearch.searchItems[0].title}
+          link={blockResultSearch.searchItems[0].link}
+        />
+      </div>
+      <div className="ui-kit__component">
+        <SimilarQueries
+          title="Related searches"
+          queries={similarQueries}
+          onClickItem={handleOnClickItemSimilarQueries}
+          viewType={viewType}
+        />
+      </div>
+      <div className="ui-kit__component">
+        <TermsBlock title={termsSuggestions.title} terms={termsSuggestions.terms} />
+      </div>
+      <div className="ui-kit__component">
+        <ImageBlock title={imageResult.title} imageUrl={imageResult.imageUrl} link={imageResult.link} />
+      </div>
+    </div>
   );
 };
 
